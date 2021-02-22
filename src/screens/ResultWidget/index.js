671ec1e1bfa8e7668155fe77db596882eceafce7
jonathan <jonathan.santosmartins@gmail.com>
2021-02-21 21:12:14 -0300

    pontuação

--- src/screens/ResultWidget/index.js
+++ src/screens/ResultWidget/index.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { Widget } from "../../components/Widget";
 import Button from "../../components/Button";
 import db from "../../../db.json";
 
 export default function ResultWidget({ results }) {
+  const [points, setPoints] = useState(0);
   const router = useRouter();
+
+  useEffect(() => {
+    setPoints(results.filter((result) => result).length * 10);
+  }, []);
+
   return (
     <>
       <Widget.Header>
@@ -13,7 +19,7 @@ export default function ResultWidget({ results }) {
       </Widget.Header>
 
       <img
-        src={db.end.images.notbad}
+        src={points < 50 ? db.end.images.notbad : db.end.images.super}
         alt="Parabéns você chegou ao fim"
         style={{
           width: "100%",
@@ -23,9 +29,7 @@ export default function ResultWidget({ results }) {
       />
 
       <Widget.Content>
-        <p>
-          Você acertou {results.filter((result) => result).length} perguntas
-        </p>
+        <p>Você fez {points} pontos</p>
         <ul>
           {results &&
             results.map((result, index) => (
